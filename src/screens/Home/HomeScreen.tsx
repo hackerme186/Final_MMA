fa349907300b6d747a6d57c900227b4b02424f9d
LAPTOP-Q462S81B\ASUS <doantrungminh20@gmail.com>
2025-07-22 10:32:48 +0700

    go

--- src/screens/Home/HomeScreen.tsx
+++ src/screens/Home/HomeScreen.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import { SafeAreaView, ScrollView, StatusBar } from 'react-native';
 import { HomeHeader } from './components/HomeHeader/HomeHeader';
-import { FeaturedAlbumsSection } from './components/sections/FeaturedAlbumsSection';
-import { MixesSection } from './components/sections/MixesSection';
-import { PlaylistGridSection } from './components/sections/PlaylistGridSection';
-import { RecentlyPlayedSection } from './components/sections/RecentlyPlayedSection';
+import { FeaturedAlbumsSection } from './components/Sections/FeaturedAlbumsSection';
+import { MixesSection } from './components/Sections/MixesSection';
+import { PlaylistGridSection } from './components/Sections/PlaylistGridSection';
+import { RecentlyPlayedSection } from './components/Sections/RecentlyPlayedSection';
 import { mockAlbums, mockMixes, mockPlaylists, mockRecentlyPlayed } from './mockHomeData';
 
 export default function HomeScreen() {
